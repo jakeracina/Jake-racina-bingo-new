2e609438e9d95f8d5932014cf45fb4d04b581dba
jakeracina <jakeracina39@gmail.com>
2025-06-15 22:31:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,6 @@ def bingo():
     return render_template_string(TEMPLATE, grid=grid)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
